e0be7c1660fc11329354f3160dabb582963bab95
Tom <tom.williams@manchester-imaging.com>
2021-10-11 15:03:28 +0100

    Created the requirements

---
+++ src/Pickles/MIL_pickles/mil_requirementsAN180.cmd
@@ -0,0 +1,18 @@
+
+SET BIN_DIR=..\Pickles.CommandLine\bin\Release\
+SET PROJ_NAME=AN180_Requirements
+
+REM generate list without exclusions to validate that all requirements have been included
+%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=1.7 --df=Word
+rename  .\Output\AssistDent.docx %PROJ_NAME%_AllRequirements.docx
+
+%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=1.7 --df=Word --et="FR;NFR"
+rename  .\Output\AssistDent.docx %PROJ_NAME%_UserRequirements.docx 
+
+%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=1.7 --df=Word --et="UR;NFR"
+rename  .\Output\AssistDent.docx %PROJ_NAME%_FunctionalRequirements.docx 
+
+%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=1.7 --df=Word --et="UR;FR"
+rename  .\Output\AssistDent.docx %PROJ_NAME%_NonFunctionalRequirements.docx 
+
+exit /b 0
