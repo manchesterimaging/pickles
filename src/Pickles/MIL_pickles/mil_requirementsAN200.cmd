bad2035db9f7b79470ec4595ecbb8cf1684a635c
Tom <tom.williams@manchester-imaging.com>
2022-06-15 14:17:51 +0100

    Generated tests and requirements for AN200

---
+++ src/Pickles/MIL_pickles/mil_requirementsAN200.cmd
@@ -0,0 +1,18 @@
+
+SET BIN_DIR=..\Pickles.CommandLine\bin\Release\
+SET PROJ_NAME=AN200_Requirements
+
+REM generate list without exclusions to validate that all requirements have been included
+%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=2.0 --df=Word
+rename  .\Output\AssistDent.docx %PROJ_NAME%_AllRequirements.docx
+
+%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=2.0 --df=Word --et="FR;NFR;WebOnly"
+rename  .\Output\AssistDent.docx %PROJ_NAME%_UserRequirements_Desktop.docx 
+
+%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=2.0 --df=Word --et="UR;NFR;WebOnly"
+rename  .\Output\AssistDent.docx %PROJ_NAME%_FunctionalRequirements_Desktop.docx 
+
+%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=2.0 --df=Word --et="UR;FR;WebOnly"
+rename  .\Output\AssistDent.docx %PROJ_NAME%_NonFunctionalRequirements_Desktop.docx 
+
+exit /b 0
