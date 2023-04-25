a2aeb5c7c80403ee8b670c41e04830204f90115c
Tom Williams <TomosGW@users.noreply.github.com>
2023-04-25 11:57:25 +0100

    Regenerated requirements and test scripts after changes as a result of #1972-CA1

--- src/Pickles/MIL_pickles/mil_requirementsAN210.cmd
+++ src/Pickles/MIL_pickles/mil_requirementsAN210.cmd
@@ -6,13 +6,13 @@ REM generate list without exclusions to validate that all requirements have been
 %BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=2.1 --df=Word
 rename  .\Output\AssistDent.docx %PROJ_NAME%_AllRequirements.docx
 
-%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=2.1 --df=Word --et="FR;NFR;WebOnly"
+%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=2.1 --df=Word --et="FR;NFR"
 rename  .\Output\AssistDent.docx %PROJ_NAME%_UserRequirements_Desktop.docx 
 
-%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=2.1 --df=Word --et="UR;NFR;WebOnly"
+%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=2.1 --df=Word --et="UR;NFR"
 rename  .\Output\AssistDent.docx %PROJ_NAME%_FunctionalRequirements_Desktop.docx 
 
-%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=2.1 --df=Word --et="UR;FR;WebOnly"
+%BIN_DIR%\Pickles.exe -f=C:\MIL_Src\docs\requirements\ -o=.\Output --sn=AssistDent --sv=2.1 --df=Word --et="UR;FR"
 rename  .\Output\AssistDent.docx %PROJ_NAME%_NonFunctionalRequirements_Desktop.docx 
 
 exit /b 0
